e56fd557f53f37f6f860608ba8d5e05419c592c0
bachas_joshi <bachasjoshi11@gmail.com>
2021-05-30 22:37:38 +0545

    bootstrap setup

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -1,10 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
      <h2>Portfolio website</h2>
+     <button className="btn btn-primary">Click</button>
     </div>
   );
 }
